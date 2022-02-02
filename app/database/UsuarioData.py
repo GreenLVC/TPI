1043a8c3c0021e3e2e2da200bb79ce9bfcd7cdaa
Franco <franconavarro1889@gmail.com>
2022-02-02 20:46:21 -0300

    comienzo capa de datos

---
+++ app/database/UsuarioData.py
@@ -0,0 +1,72 @@
+import pymysql
+from database.connection import DataBase
+from models import Usuario
+from typing import List, Optional
+
+
+class UsuarioData(DataBase):
+    def __init__(self):
+        super().__init__()
+
+    def GetOne(self, nombre, contrasenia) -> Optional[Usuario]:
+        self.open()
+        try:
+            self.cursor.execute(
+                "select * from usuarios where alias=%s and contrasenia =%s", nombre, contrasenia)
+            u = Usuarios(*self.cursor.fetchone().values())
+            return u
+        except:
+            print("No se pudo recuperar el usuario.")
+            self.connection.rollback()
+            return None
+
+        finally:
+            self.cursor.close()
+            self.close()
+
+    def GetAll(self) -> List[Usuario]:
+        self.open()
+        listaUsuarios = list()
+        try:
+            self.cursor.execute("select * from usuarios",)
+            for usu in self.cursor.fetchall():
+                u = Usuarios(*usu.values())
+                listaUsuarios.append(u)
+            return listaUsuarios
+
+        except:
+            print("No se pudo recuperar el usuario.")
+            self.connection.rollback()
+        finally:
+            self.cursor.close()
+            self.close()
+
+    def register(self, usuario: Usuario) -> bool:
+        insertcmd = "insert into usuarios(alias, contrasenia, foto_perfil, pais, idCuenta) values (%s, %s, %s, %s, %s)"
+        self.open()
+        try:
+            self.cursor.execute(insertcmd, usuario.lst())
+            self.connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        finally:
+            self.cursor.close()
+            self.close()
+
+    def update(self, user: Usuario) -> bool:
+        updt = "update usuarios set contrasenia= %s, alias= %s, foto_perfil= %s, pais= %s , idCuenta =%s where alias= %s and contrasenia =%"
+        params = (user.contrasenia, user.alias,
+                  user.foto_perfil, user.pais, user.id_cuenta, user.alias, user.contrasenia)
+        self.open()
+        try:
+            self.cursor.execute(updt, params)
+            self.connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        finally:
+            self.cursor.close()
+            self.connection.close()
--- app/database/connection.py
+++ app/database/connection.py
@@ -1,59 +1,17 @@
-from contextlib import contextmanager
-from typing import Any, Iterator, List, Optional
+import pymysql
 
-import sqlite3
+class DataBase:
+    def __init__(self):
+        self.connection = pymysql.connect(host='localhost',
+                                          user='root',
+                                          password='1234',
+                                          db='myplaylists')
 
-from config import Config
-from .security import _encrypt_decrypt
+        self.close()
 
-DATABASE_PATH = Config.DATABASE_PATH
+    def close(self):
+        self.connection.close()
 
-
-@contextmanager
-@_encrypt_decrypt
-def __get_cursor() -> Iterator[sqlite3.Cursor]:
-    connection: sqlite3.Connection = sqlite3.connect(DATABASE_PATH)
-    cursor: sqlite3.Cursor = connection.cursor()
-
-    #ojo porque lo anterior es solo para sqlite
-    try:
-        yield cursor
-        connection.commit()
-    finally:
-        cursor.close()
-        connection.close()
-
-
-def _fetch_one(query: str, parameters: Optional[List[str]] = None) -> Any:
-    if parameters is None:
-        parameters = []
-
-    with __get_cursor() as cursor:
-        cursor.execute(query, parameters)
-        return cursor.fetchone()
-
-
-def _fetch_all(query: str, parameters: Optional[List[str]] = None) -> List:
-    if parameters is None:
-        parameters = []
-
-    with __get_cursor() as cursor:
-        cursor.execute(query, parameters)
-        return cursor.fetchall()
-
-
-def _fetch_none(query: str, parameters: Optional[List[str]] = None) -> None:
-    if parameters is None:
-        parameters = []
-
-    with __get_cursor() as cursor:
-        cursor.execute(query, parameters)
-
-
-def _fetch_lastrow_id(query: str, parameters: Optional[List[str]] = None) -> int:
-    if parameters is None:
-        parameters = []
-
-    with __get_cursor() as cursor:
-        cursor.execute(query, parameters)
-        return cursor.lastrowid
+    def open(self):
+        self.connection.connect()
+        self.cursor = self.connection.cursor(pymysql.cursors.DictCursor)
